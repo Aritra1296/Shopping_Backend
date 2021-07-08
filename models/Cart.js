483f608bdd366aca61e66d3685ee2adf89531ec7
Aritra1296 <aritrivu@gmail.com>
2021-07-08 18:10:30 +0530

    Cart Model class created

---
+++ models/Cart.js
@@ -0,0 +1,34 @@
+const mongoose = require('mongoose')
+
+const cartSchema = mongoose.Schema({
+  userId: {
+    type: String,
+    required: true,
+  },
+  productName: {
+    type: String,
+    required: true,
+  },
+  productDescription: {
+    type: String,
+    required: true,
+  },
+  productImage: {
+    type: String,
+    required: true,
+  },
+  productCount: {
+    type: String,
+    required: true,
+  },
+  total: {
+    type: String,
+    required: true,
+  },
+  date: {
+    type: Date,
+    default: Date.now,
+  },
+})
+
+module.exports = mongoose.model('Cart', cartSchema)
